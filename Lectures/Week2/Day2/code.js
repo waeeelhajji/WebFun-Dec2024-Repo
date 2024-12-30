229e709d49988de5c45da1c6a355a04b32fdaf5d
waeeelhajji <waelhajji.contact@gmail.com>
2024-12-30 10:36:17 +0100

    week3Day1

---
+++ Lectures/Week2/Day2/code.js
@@ -0,0 +1,22 @@
+/* 
+Given an array of numbers,
+return a count of how many are both even and negative.
+*/
+
+const nums1 = [1, 5, -1, 2, -4, 9, -10, 0, -3, -2];
+const expected1 = 3;
+
+const nums2 = [];
+const expected2 = 0;
+
+const nums3 = [-4, -2, -6];
+const expected3 = 3;
+
+/**
+ * Counts how many numbers are both even and negative.
+ * @param {number} nums
+ * @returns {number} The count.
+ */
+function countEvenNegatives(nums) {}
+
+/*****************************************************************************/
